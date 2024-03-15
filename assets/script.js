03746475dba6ba25679b6cef868391c3099f4fd7
Kevin-0 <xianlongqiu.dev@gmail.com>
2024-03-15 19:30:43 +0100

    corretto gli errori, aggiunto sezione curriculum e meta open graph

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", () => {
+  emailjs.init({
+    publicKey: "gcxIGCyDC6xpN93UL",
+  });
+});
+
 function sendMail() {
   let name = document.getElementById("nome").value;
   let cognome = document.getElementById("cognome").value;
@@ -10,22 +16,17 @@ function sendMail() {
     message_html: message,
   };
   if (email === "") {
-    alert(
-      "Compila tutto. Riprova più tardi."
-    );
+    alert("Compila tutto. Riprova più tardi.");
     console.error("Email non può essere vuota.");
     return false; // Non eseguire l'invio se l'email è vuota
   }
   emailjs.send("service_uwkpzk2", "template_zulapcq", params).then(
-    
     function (response) {
       alert("Email inviata con successo!");
       console.log("SUCCESS!", response.status, response.text);
     },
     function (error) {
-      alert(
-        "Compila tutto. Riprova più tardi."
-      );
+      alert("Compila tutto. Riprova più tardi.");
       console.log("FAILED...", error);
     }
   );
